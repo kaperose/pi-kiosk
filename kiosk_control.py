6df78dbda7a7f8ce485dedcb76b0b31147cb46ec
kaperose <150841704+kaperose@users.noreply.github.com>
2025-10-31 16:42:38 +0100

    a

--- kiosk_control.py
+++ kiosk_control.py
@@ -112,7 +112,7 @@ def launch_browser(urls):
     
     command = [
         'chromium',
-        #S'--kiosk',
+        '--kiosk',
         '--disable-infobars',
         '--noerrdialogs',
         '--check-for-update-interval=31536000',
